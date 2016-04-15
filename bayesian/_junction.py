68858e5590d8925fea13be5ba6fb3083d7d8405c
Samuel <sam.deslauriers@gmail.com>
2016-04-15 15:25:17 -0400

    Adds the JunctionTree class used to compute marginals

---
+++ bayesian/_junction.py
@@ -0,0 +1,233 @@
+from copy import copy
+
+import bayesian
+
+class Bucket(object):
+    def __init__(self, variables):
+        """Bucket in a junction tree
+        
+        Args:
+            variables (set of bayesian.Variables) : The set of variables
+                contained in the bucket.
+                
+        """
+
+        # The variables of the bucket. 
+        self.variables = variables 
+
+        # The separator above the bucket.
+        self.out = None
+
+        # The separators below the bucket.
+        self.separators = []
+        
+        # The probability tables of the bucket.
+        self.tables = []
+
+class Separator(object):
+    def __init__(self, variables):
+        """Separator in a junction tree
+        
+        Args:
+            variables (set of bayesian.Variables) : The set of variables
+                contained in the bucket.
+
+        """
+
+        # The variables of the separator.
+        self.variables = variables 
+
+        # The up/downbound messages.
+        self.upbound = None 
+        self.downbound = None
+
+class JunctionTree(object):
+    def __init__(self, network):
+        """Junction tree of a bayesian network
+        
+        Args:
+            network (bayesian.Network) : The Bayesian network used to compute
+                the domain graph.
+        """
+
+        # The separators and buckets of the junction tree.
+        self.separators = []
+        self.buckets = []
+
+        # Keep the network.
+        self._network = network
+
+        # Compute the domain graph and use it to initialize the junction
+        # tree.
+        domain_graph = bayesian.DomainGraph(network)
+        self._build_from_graph(domain_graph)
+
+        # Prepare to compute marginals.
+        self._collect_evidence()
+        self._distribute_evidence()
+    
+    @property
+    def network(self):
+        """Get the Bayesian network of the junction tree"""
+        return self._network
+    
+    def _collect_evidence(self):
+        """Collect the evidence to the root node"""
+        
+        for bucket in self.buckets:
+
+            if bucket.out is not None:
+                tables = list(bucket.tables)
+
+                # Collect the messages of the separators that are below the 
+                # the bucket.
+                for separator in bucket.separators:
+                    tables.append(separator.upbound)
+                
+                # Compute the product of all tables and eliminate the variables
+                # that are not in the destination separator.
+                new_table = tables[0]
+                for table in tables[1:]:
+                    new_table = new_table * table
+
+                separator = bucket.out
+                to_remove = []
+                for variable in new_table.domain:
+                    if variable not in separator.variables: 
+                        new_table = new_table.marginalize(variable)
+                
+                separator.upbound = new_table
+
+    def _distribute_evidence(self):
+        """Distribute the evidence from the root node"""
+
+        for bucket in reversed(self.buckets):
+            bucket_tables = list(bucket.tables)
+
+            # Collect the messages of the separator that is above
+            # the bucket.
+            if bucket.out is not None:
+                bucket_tables.append(bucket.out.downbound)
+
+            # Distribute the messages to the separators below the bucket.
+            for separator in bucket.separators:
+                tables = list(bucket_tables)
+
+                # Add the message from all other separators.
+                for other_separator in bucket.separators:
+                    if other_separator is not separator:
+                        tables.append(other_separator.upbound)
+                
+                # Compute the product of all tables.
+                new_table = tables[0]
+                for table in tables[1:]:
+                    new_table = new_table * table
+    
+                for variable in new_table.domain:
+                    if variable not in separator.variables:
+                        new_table = new_table.marginalize(variable)
+                
+                separator.downbound = new_table
+                
+    def marginals(self):
+        """Compute all marginals"""
+       
+        variables = self.network.domain
+
+        marginals = []
+        for variable in variables:
+
+            # Find a bucket with the variable in its domain.
+            for bucket in self.buckets:
+                if variable in bucket.variables:
+                    break
+
+            # Get all the messages from the separators.
+            tables = list(bucket.tables)
+            if bucket.out is not None:
+                tables.append(bucket.out.downbound)
+            for separator in bucket.separators:
+                tables.append(separator.upbound)
+            
+            # Compute the product of all tables.
+            new_table = tables[0]
+            for table in tables[1:]:
+                new_table = new_table * table
+
+            # Marginalize all variables except the current objective.
+            for bucket_variable in bucket.variables:
+                if bucket_variable is not variable:
+                    new_table = new_table.marginalize(bucket_variable)
+            
+            marginals.append(new_table)
+
+        return marginals
+    
+    def _build_from_graph(self, graph):
+        """Get the junction tree of the graph"""
+
+        # Create a copy to the domain graph to be able to modify it.
+        graph_copy = copy(graph)
+
+        # The number of nodes removed is used as a bucket and separator
+        # index.
+        nb_nodes_removed = 0
+        
+        # The list of used tables.
+        used_tables = set() 
+
+        done = False
+        while not done:
+
+            # Find a simplicial node.
+            simplicial_node = graph_copy.simplicial_node
+            family = simplicial_node.family
+            bucket = Bucket(set([node.data for node in family]))
+            self.buckets.append(bucket)
+           
+            if len(family) < len(graph_copy._nodes):
+                
+                # Find all nodes who have heighbors only in the family.
+                nodes_to_remove = set() 
+                nodes_to_keep = set() 
+                for node in family:
+                    if node.family <= family:
+                        nodes_to_remove.add(node)
+                        nb_nodes_removed = nb_nodes_removed + 1
+                    else:
+                        nodes_to_keep.add(node)
+                
+                # Remove them from the graph.
+                for node in nodes_to_remove:
+                    graph_copy.remove_node(node)
+
+                # The remaining nodes are part of a separator.
+                separator = Separator(set([n.data for n in nodes_to_keep]))
+                separator.index = nb_nodes_removed 
+                separator.downward = bucket
+                bucket.out = separator
+                self.separators.append(separator)
+                
+                # Update the index of the bucket.
+                bucket.index = nb_nodes_removed 
+            else:
+                nodes_to_remove = family
+                bucket.index = len(graph._nodes)
+                done = True
+
+            # Find the tables that contains the variables removed from the
+            # graph and add them to the bucket.
+            variables = [node.data for node in nodes_to_remove]
+            possible_tables = graph_copy.network.get_tables(variables)
+            bucket.tables = list(set(possible_tables) - used_tables)
+            used_tables = used_tables | set(bucket.tables)
+
+        # Connect the separators to their upward bucket.
+        for separator in self.separators:
+            separator.upward = None
+            for bucket in self.buckets:
+                if bucket.index > separator.index and \
+                        separator.variables <= bucket.variables:
+                    separator.upward = bucket
+                    bucket.separators.append(separator)
+                    break
---
+++ bayesian/tests/test_junction_junctiontree.py
@@ -0,0 +1,41 @@
+import unittest
+import time
+
+import bayesian
+import bayesian.tests.networks
+
+class TestGraphJunctionTree(unittest.TestCase):
+    """Test the JunctionTree class"""
+
+    def test_marginals(self):
+        """Test the marginals method"""
+
+        network = bayesian.tests.networks.Pyramid()
+
+        # Compute the marginals using the junction tree.
+        junction_tree = bayesian.JunctionTree(network)
+        junction_tree_marginals = junction_tree.marginals()
+
+        # The marginals computed should be equal to the marginals computed 
+        # one by one.
+        for junction_tree_marginal in junction_tree_marginals:
+            marginal = network.marginal(junction_tree_marginal.domain[0])
+            self.assertAlmostEqual(junction_tree_marginal[0], marginal[0])
+            self.assertAlmostEqual(junction_tree_marginal[1], marginal[1])
+ 
+        # Computing the marginals using the junction tree should be faster.
+        t0 = time.time()
+        junction_tree.marginals()
+        t1 = time.time()
+        junction_tree_time = t1 - t0
+
+        t0 = time.time()
+        for junction_tree_marginal in junction_tree_marginals:
+            network.marginal(junction_tree_marginal.domain[0])
+        t1 = time.time()
+        one_by_one_time = t1 - t0
+
+        self.assertTrue(junction_tree_time < one_by_one_time)
+
+if __name__ == '__main__':
+    unittest.main()
