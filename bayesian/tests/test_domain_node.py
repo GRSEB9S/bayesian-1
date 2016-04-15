5f0903d60b3fb847c8b36ab39d444423e8176b25
Samuel <sam.deslauriers@gmail.com>
2016-04-15 15:27:12 -0400

    Renames test files to match new package names

--- bayesian/tests/test_domain_node.py
+++ bayesian/tests/test_domain_node.py
@@ -1,18 +1,20 @@
 import unittest
-import bayesian._graph
+
+import bayesian
+from bayesian._domain import Node
 
 class TestNode(unittest.TestCase):
 
     def test_constructor(self):
         """Test the Node constructor"""
 
-        node1 = bayesian._graph.Node('A')
+        node1 = Node('A')
 
     def test_add_link(self):
         """Test the add_link method"""
 
-        node1 = bayesian._graph.Node('A')
-        node2 = bayesian._graph.Node('B')
+        node1 = Node('A')
+        node2 = Node('B')
 
         # Adding a link between node1 and node2 should also create a link 
         # between node2 and node1.
@@ -23,10 +25,10 @@ class TestNode(unittest.TestCase):
     def test_is_simplical(self):
         """Test the is_simplicial property"""
 
-        node1 = bayesian._graph.Node('A')
-        node2 = bayesian._graph.Node('B')
-        node3 = bayesian._graph.Node('C')
-        node4 = bayesian._graph.Node('D')
+        node1 = Node('A')
+        node2 = Node('B')
+        node3 = Node('C')
+        node4 = Node('D')
 
         # Add the links. There are no links between nodes 1 and 3.
         node1.add_link(node2)
